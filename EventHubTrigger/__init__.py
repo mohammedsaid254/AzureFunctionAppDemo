893aa6adc4498cb5aff8547676b57deb5e55dcd1
mohammedsaid254 <mohammedsaid254@gmail.com>
2022-08-15 18:07:23 +0200

    Update __init__.py

--- EventHubTrigger/__init__.py
+++ EventHubTrigger/__init__.py
@@ -4,5 +4,5 @@ import azure.functions as func
 
 
 def main(event: func.EventHubEvent):
-    logging.info('Python EventHub trigger processed an event: %s',
+    logging.info('======= Python EventHub trigger processed an event: %s',
                  event.get_body().decode('utf-8'))
